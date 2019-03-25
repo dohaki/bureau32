312d7c011023ae0b98ccd75c83cdf837f39e8c4e
dakingha69 <dongha.kim210@gmail.com>
2019-03-25 17:01:27 +0100

    Add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const ContactPage = () => (
+  <div style={{
+    height: '100%',
+    display: 'flex',
+  }}>
+    <div
+      style={{
+        maxWidth: '40rem',
+        height: 'auto',
+        display: 'block',
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        marginTop: '6.3rem',
+        marginBottom: 'auto',
+        textAlign: 'center',
+      }}
+    >
+      <p>bureau32 - office for architectural visualization</p>
+
+      <p style={{ margin: 0 }}>weserstr. 32</p>
+      <p style={{ margin: 0 }}>12045 berlin</p>
+      <p>for collaboration and inquiries:</p>
+      <p style={{ margin: 0 }}>info@bureau32.de</p>
+      <p style={{ margin: 0 }}>+49 173 8597837</p>
+    </div>
+  </div>
+)
+
+export default ContactPage
