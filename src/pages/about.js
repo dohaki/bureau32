4a174b012a788f6e4d714a71c7bce46352b32072
dakingha69 <dongha.kim210@gmail.com>
2019-03-25 17:29:02 +0100

    Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const AboutPage = () => (
+  <div
+    style={{
+      height: '100%',
+      display: 'flex',
+    }}
+  >
+    <div
+      style={{
+        maxWidth: '40rem',
+        height: 'auto',
+        display: 'block',
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        marginTop: '6.3rem',
+        marginBottom: 'auto',
+        textAlign: 'center',
+      }}
+    >
+      <p>bureau32 is a practice dedicated to the visualization of unbuilt things.</p>
+    </div>
+  </div>
+)
+
+export default AboutPage
